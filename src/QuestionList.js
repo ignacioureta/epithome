69d32cc8b0cf2eef2f6df2e6ce5dc1906c3959a6
Cristian Fraser <cristian.fraser@gmail.com>
2018-09-08 17:55:51 -0300

    add selection

--- src/QuestionList.js
+++ src/QuestionList.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
+import classNames from 'classnames'
 
 import questions from './questions';
 
 class QuestionList extends Component {
+  state = {
+    selectedItem: null,
+  }
+
+  handleSelect = (index) => {
+    this.setState({
+      selectedItem: index,
+    })
+  }
+
   render() {
+    const { selectedItem } = this.state
+
     return (
       <ul className="questionList">
         {questions.map((question, idx) => (
-          <li className="questionItem" key={question + idx}>
-            <div className="questionNumber">{idx + 1}</div>
+          <li
+            className="questionItem"
+            key={question + idx}
+            onClick={() => { this.handleSelect(idx) }}
+          >
+            <div className={classNames('questionNumber', { selectedItem: selectedItem === idx } )}>{idx + 1}</div>
             <div className="questionText">{question}</div>
           </li>
         ))}
